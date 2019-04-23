c2db00906909ba5fb2c9662a726dc9a597cab3f4
Arpan Patel <ap.patel1893@gmail.com>
2019-04-23 17:05:48 -0500

    revert state

--- hooks-01-starting-project/src/components/Todo.js
+++ hooks-01-starting-project/src/components/Todo.js
@@ -1,30 +1,32 @@
 import React, { useState }from 'react';
 
 const todo = props => {
-    // const [todoName, setTodoName] = useState('');
-    // const [todoList, setTodolist] = useState([]);
+    const [todoName, setTodoName] = useState('');
+    const [todoList, setTodolist] = useState([]);
 
-    const [todoState, setTodoState] = useState({userInput: '', todoList: []});
+    // const [todoState, setTodoState] = useState({userInput: '', todoList: []});
 
     const inputChangeHandler = (event) => {
-        setTodoState({
-            userInput: event.target.value,
-            todoList: todoState.todoList});
+        // setTodoState({
+        //     userInput: event.target.value,
+        //     todoList: todoState.todoList});
+        setTodoName(event.target.value);
     };
 
     const todoAddHandler = () => {
-        setTodoState({
-            userInput: todoState.userInput, 
-            todoList: todoState.todoList.concat(todoState.userInput)
-        }) ;
+        // setTodoState({
+        //     userInput: todoState.userInput, 
+        //     todoList: todoState.todoList.concat(todoState.userInput)
+        // }) ;
+        setTodolist(todoList.concat(todoName));
     };
 
     return (
         <React.Fragment>
-            <input type='text' placeholder='Todo' onChange={inputChangeHandler} value={todoState.userInput} />
+            <input type='text' placeholder='Todo' onChange={inputChangeHandler} value={todoName} />
             <button type='button' onClick={todoAddHandler}>Add</button>
             <ul>
-                {todoState.todoList.map(todo => (
+                {todoList.map(todo => (
                     <li key={todo}>{todo}</li>
                 ))}
 
