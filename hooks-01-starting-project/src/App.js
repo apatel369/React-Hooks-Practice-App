d0e155f1555b248255a0ea35906a2c14f23b40ec
Arpan Patel <ap.patel1893@gmail.com>
2019-04-25 22:39:09 -0500

    format

--- hooks-01-starting-project/src/App.js
+++ hooks-01-starting-project/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import Todo from './components/Todo'
+import React, { Component } from "react";
+import Todo from "./components/Todo";
 
 class App extends Component {
   render() {
