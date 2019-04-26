433383e45f66ed6db6e36cb52fafad05460db81b
Arpan Patel <ap.patel1893@gmail.com>
2019-04-25 22:37:49 -0500

    format code

--- hooks-01-starting-project/src/components/Todo.js
+++ hooks-01-starting-project/src/components/Todo.js
@@ -1,38 +1,46 @@
-import React, { useState }from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
 const todo = props => {
-    const [todoName, setTodoName] = useState('');
-    const [todoList, setTodolist] = useState([]);
+  const [todoName, setTodoName] = useState("");
+  const [todoList, setTodolist] = useState([]);
 
-    // const [todoState, setTodoState] = useState({userInput: '', todoList: []});
+  // const [todoState, setTodoState] = useState({userInput: '', todoList: []});
 
-    const inputChangeHandler = (event) => {
-        // setTodoState({
-        //     userInput: event.target.value,
-        //     todoList: todoState.todoList});
-        setTodoName(event.target.value);
-    };
+  const inputChangeHandler = event => {
+    // setTodoState({
+    //     userInput: event.target.value,
+    //     todoList: todoState.todoList});
+    setTodoName(event.target.value);
+  };
 
-    const todoAddHandler = () => {
-        // setTodoState({
-        //     userInput: todoState.userInput, 
-        //     todoList: todoState.todoList.concat(todoState.userInput)
-        // }) ;
-        setTodolist(todoList.concat(todoName));
-    };
+  const todoAddHandler = () => {
+    // setTodoState({
+    //     userInput: todoState.userInput,
+    //     todoList: todoState.todoList.concat(todoState.userInput)
+    // }) ;
+    setTodolist(todoList.concat(todoName));
+    // axios.post
+  };
 
-    return (
-        <React.Fragment>
-            <input type='text' placeholder='Todo' onChange={inputChangeHandler} value={todoName} />
-            <button type='button' onClick={todoAddHandler}>Add</button>
-            <ul>
-                {todoList.map(todo => (
-                    <li key={todo}>{todo}</li>
-                ))}
-
-            </ul>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <input
+        type="text"
+        placeholder="Todo"
+        onChange={inputChangeHandler}
+        value={todoName}
+      />
+      <button type="button" onClick={todoAddHandler}>
+        Add
+      </button>
+      <ul>
+        {todoList.map(todo => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
 };
 
 export default todo;
